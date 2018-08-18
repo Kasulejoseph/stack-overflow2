43abb811d0c7740da9e972a3a95303c28021558f
kasulejoseph <kasule06joseph@gmail.com>
2018-08-18 14:01:21 +0300

    [#159853086] post a question

--- app/question.py
+++ app/question.py
@@ -10,12 +10,22 @@ Questions = question()
 def questions():
     return jsonify({"questions": Questions})
 
-<<<<<<< HEAD
 #get a question
 @app.route('/api/v1/questions/<string:id>/', methods=['GET'])
 def one_question(id):
     qtn = [question for question in Questions if question['id']== int(id)]
     return jsonify({'question': qtn})
 
-=======
->>>>>>> get_all_questions
+#post a questions
+@app.route('/api/v1/questions/', methods=['POST'])
+def add_questions():
+    data = request.get_json()
+    question ={
+        "id": data['id'],
+        "question": data['question'],
+        "author": data['author'],
+        "date_created": data['date_created'],
+    }
+    Questions.append(question)
+
+    return jsonify({'question': Questions})
