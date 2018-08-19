e7302820fc9883dcc430d923874845c75d77bc27
kasulejoseph <kasule06joseph@gmail.com>
2018-08-19 18:50:19 +0300

    modified config and run files

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-from app import app
-from app import app_config
+import os
+
+from app import create_app
+config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
